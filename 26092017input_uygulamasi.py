e4a560f5fefde345af9608ed91dd314170843a75
Sunrock14 <farukkocademir@hotmail.com>
2017-09-26 13:13:04 +0300

    ilk commit

---
+++ 26092017input_uygulamasi.py
@@ -0,0 +1,9 @@
+#Kullanıcıdan değeri al ve ekrana bas
+
+isim=input("Merhaba. Adın nedir?")
+print("Merhaba,",isim,". Şimdi senden iki sayı girmeni isteyeceğim. Daha sonra bu sayıların kareleri toplamını bulacağım")
+
+a=int(input("İlk sayiyi gir : "))
+b=int(input("İkinci sayiyi gir : "))
+karelerToplami=(a*a)+(b*b)
+print("Girdiginiz sayilarin kareleri toplami = ",karelerToplami)
---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+
+a=5
+b=12
+print("Hello World!")
+print(a+2)    # a'nın 2 fazlası yazdırılıyor. a degismez!
+print(a,"+",b,"=",a+b)
+
+# Aşağıdaki iki satırda sayı olmayan değişken yazdırılıyor
+isim="Faruk"
+print("Merhaba ",isim)
